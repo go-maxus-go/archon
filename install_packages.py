3fbec144f79c258cad3ee7ed708de88895442cfc
Maxim Sheshukov <sheshukovmaxim@gmail.com>
2021-09-15 14:39:27 +0200

    Add commands

--- install_packages.py
+++ install_packages.py
@@ -1,6 +1,7 @@
 import os
 
 packages = []
+commands = []
 
 file = open("packages.txt", "r")
 data = file.read()
@@ -10,8 +11,17 @@ lines = data.splitlines()
 for line in lines:
     line = line.split("#")[0]
     line = line.strip()
-    if line != "":
+    if line == "":
+        continue
+
+    if line[0] == "$":
+        line = line[1:].strip()
+        commands.append(line)
+    else:
         packages.append(line)
 
 packages = " ".join(packages)
 os.system("sudo pacman --noconfirm -S {0}".format(packages))
+
+for command in commands:
+    os.system(command)
