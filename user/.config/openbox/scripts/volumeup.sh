e966f1e3e2a007ac01363bd99e8f2b8e7e27f3d4
Maxim Sheshukov <sheshukovmaxim@gmail.com>
2021-09-17 16:28:48 +0200

    Changing of volume unmutes sound

--- user/.config/openbox/scripts/volumeup.sh
+++ user/.config/openbox/scripts/volumeup.sh
@@ -10,5 +10,10 @@ fi
 
 volume="$volume%"
 
+if [ -z "$(pactl get-sink-mute @DEFAULT_SINK@ | grep no)" ]
+then
+    pactl set-sink-mute @DEFAULT_SINK@ false
+fi
+
 pactl set-sink-volume @DEFAULT_SINK@ $volume
 dunstify "Volume $volume" -u 0 -r 92347
