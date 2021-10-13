b293e456cddfd9395097e85ae5f1a91178887408
Maxim Sheshukov <sheshukovmaxim@gmail.com>
2021-10-13 13:04:08 +0200

    Add brightness shortcuts

---
+++ user/.config/archon/scripts/brightness_up.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+maxBrightness="$(brightnessctl m)"
+minBrightness=1
+
+currBrightness="$(brightnessctl g)"
+newBrightness=$(($currBrightness+$maxBrightness*5/100))
+
+if [ "$newBrightness" -lt "$minBrightness" ]
+then
+    newBrightness=$minBrightness
+fi
+
+if [ "$newBrightness" -gt "$maxBrightness" ]
+then
+    newBrightness=$maxBrightness
+fi
+
+newBrightnessPercent=$((100*$newBrightness/$maxBrightness))
+
+brightnessctl s $newBrightness
+dunstify "Brightness $newBrightnessPercent%" -u 0 -r 92347
