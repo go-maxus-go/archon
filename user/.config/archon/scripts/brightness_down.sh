9f3257b4f4d853f54c1e7e1212145e1049035de2
Maxim Sheshukov <sheshukovmaxim@gmail.com>
2021-10-13 20:40:46 +0200

    Improve brightness control

--- user/.config/archon/scripts/brightness_down.sh
+++ user/.config/archon/scripts/brightness_down.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
+brightness="$(brightnessctl g)"
 maxBrightness="$(brightnessctl m)"
-minBrightness=1
+softBrightness=$(xrandr --verbose | awk '/Brightness/ { print $2; exit }')
 
-currBrightness="$(brightnessctl g)"
-newBrightness=$(($currBrightness-$maxBrightness*5/100))
+displays=$(xrandr | grep " connected" | sed  "s/connected/\n/" | grep "^[^()]*$")
 
-if [ "$newBrightness" -lt "$minBrightness" ]
-then
-    newBrightness=$minBrightness
-fi
-
-if [ "$newBrightness" -gt "$maxBrightness" ]
-then
-    newBrightness=$maxBrightness
-fi
-
-newBrightnessPercent=$((100*$newBrightness/$maxBrightness))
-
-brightnessctl s $newBrightness
-dunstify "Brightness $newBrightnessPercent%" -u 0 -r 92347
+dir=$(cd "$(dirname "$0")" && pwd)
+python $dir/brightness_down.py $brightness $maxBrightness $softBrightness $displays
