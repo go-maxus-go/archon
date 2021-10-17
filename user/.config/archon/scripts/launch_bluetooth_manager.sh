cabb86515c728e6d3087e80ec4ba0a35093bf26d
Maxim Sheshukov <sheshukovmaxim@gmail.com>
2021-10-17 22:10:35 +0200

    Fix bluetooth script

--- user/.config/archon/scripts/launch_bluetooth_manager.sh
+++ user/.config/archon/scripts/launch_bluetooth_manager.sh
@@ -1,3 +1,10 @@
-bluetoothctl -- power on &&
-blueman-manager &&
-bluetoothctl -- power off
+pid=$(pgrep blueman-manager)
+isNumber='^[0-9]+$'
+if ! [[ $pid =~ $isNumber ]] ; then
+    bluetoothctl -- power on &&
+    blueman-manager &&
+    bluetoothctl -- power off &&
+    pkill blueman-*
+else
+    blueman-manager
+fi
