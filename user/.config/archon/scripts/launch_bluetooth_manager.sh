d9cc4b93da20275b0ee52b478be57739a43bcf4a
Maxim Sheshukov <sheshukovmaxim@gmail.com>
2021-10-21 21:00:28 +0200

    Change blueman to blueberry

--- user/.config/archon/scripts/launch_bluetooth_manager.sh
+++ user/.config/archon/scripts/launch_bluetooth_manager.sh
@@ -1,10 +1,21 @@
-pid=$(pgrep blueman-manager)
-isNumber='^[0-9]+$'
-if ! [[ $pid =~ $isNumber ]] ; then
-    bluetoothctl -- power on &&
-    blueman-manager &&
-    bluetoothctl -- power off &&
-    pkill blueman-*
+#!/bin/bash
+
+pid=$(pidof blueberry)
+if [ -z "$pid" ]
+then
+    bluetoothctl -- power on && blueberry
+    for i in {1..10}; do
+        sleep 1
+        pid=$(pidof blueberry)
+        if [ -z "$pid" ]
+        then
+            :
+        else
+            tail --pid=$pid -f /dev/null
+            break
+        fi
+    done
+    bluetoothctl -- power off
 else
-    blueman-manager
+    blueberry
 fi
