a49a01813de16254360f83d4a5d2f9bb3bfc0c70
Maxim Sheshukov <sheshukovmaxim@gmail.com>
2024-04-22 20:57:14 +0200

    Add Hibernate option to the power menu

--- user/.config/archon/scripts/open_power_menu.sh
+++ user/.config/archon/scripts/open_power_menu.sh
@@ -5,15 +5,19 @@ rofi_command="rofi -theme $dir/config.rasi"
 
 # Options
 shutdown="Shutdown"
+hibernate="Hibernate"
 reboot="Restart"
 logout="Log Out"
 
-options="$shutdown\n$reboot\n$logout"
+options="$shutdown\n$hibernate\n$reboot\n$logout"
 
 chosen="$(echo -e "$options" | $rofi_command -dmenu)"
 case $chosen in
     $shutdown)
 		shutdown now
+        ;;
+    $hibernate)
+		systemctl hibernate
         ;;
     $reboot)
 		systemctl reboot
